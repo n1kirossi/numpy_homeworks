31bc452e7ca2071ca98d5771adc9b42e360f2d5a
Покровский Михаил Александрович <misha.pokrovskiy@list.ru>
2025-01-22 15:31:20 +0300

    Домашнее задание 2 завершено

--- hmwrk2.py
+++ hmwrk2.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-## 1. Что надо изменить в последнем примере, чтобы он заработал без ошибок (транслирование)?
+
+# 1. Что надо изменить в последнем примере, чтобы он заработал без ошибок (транслирование)?
 
 a = np.ones((3, 2))
 b = np.arange(3)
@@ -16,10 +17,16 @@ b = np.arange(3).reshape(3, 1)
 c = a + b
 print(c)
 
-## 2. Пример для y. Вычислить количество элементов (по обоим размерностям), значения которых больше 3 и меньше 9
+# 2. Пример для y. Вычислить количество элементов (по обеим размерностям), значения которых больше 3 и меньше 9
 
 y = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
 
-print(np.sum(y, axis=0))
-print(np.sum(y, axis=1))
-print(np.sum(y))
+conditions = np.logical_and(y > 3, y < 9)
+
+print(np.sum(conditions, axis=0))
+print(np.sum(conditions, axis=1))
+
+# или
+
+print(np.sum(conditions, axis=0).sum())
+print(np.sum(conditions, axis=1).sum())
